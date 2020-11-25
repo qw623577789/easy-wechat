ebfe6270a52d8b5d41b2f5de4586a111264464b3
少喝肥宅水 <718399822@qq.com>
2020-11-25 21:05:19 +0800

    feat: 公众号-获取带参数二维码

---
+++ src/core/platform/qr_code/index.js
@@ -0,0 +1,40 @@
+const Base = require('../../base.js');
+
+module.exports = class extends Base {
+    constructor(logger, config = undefined) {
+        super(logger, config);
+
+    }
+
+    async get({ scene, permanent = false, expireSeconds = 60 * 60 * 24 }) {
+        let queryData = {
+            ...permanent
+                ? {
+                    "action_name": sceneId
+                        ? `QR_LIMIT_SCENE`
+                        : `QR_LIMIT_STR_SCENE`,
+                }
+                : {
+                    "expire_seconds": expireSeconds,
+                    "action_name": sceneId
+                        ? `QR_SCENE`
+                        : `QR_STR_SCENE`,
+                },
+            "action_info": {
+                "scene": typeof scene === "number"
+                    ? { "scene_id": scene }
+                    : { "scene_str": scene }
+            }
+        }
+
+        let accessToken = await this.config.context.platform.accessToken();
+        let response = await this.request.post
+            .url(`https://api.weixin.qq.com/cgi-bin/qrcode/create?access_token=${accessToken}`)
+            .json(queryData)
+            .execute();
+
+        let { ticket, expire_seconds, url } = await this.commonResponseJsonParse(response);
+
+        return `https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${encodeURIComponent(ticket)}`
+    }
+}
